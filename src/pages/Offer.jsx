5b0ed1fe6d95da7a8433fac9688dbf07e638fb54
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-08-01 20:54:01 +0200

    content

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -8,90 +8,52 @@ const Offer = () => {
     <div
       name="offer"
       className="d-flex flex-column justify-content-center align-items-center component-style"
-      style={{ height: "87vh" }}
+      style={{ height: "150vh" }}
     >
-      <h1 className="heading-custom">Oferta</h1>
+      <h1 className="heading-custom mb-5 mt-5">Oferta</h1>
       <Container>
         <Row className="justify-content-center">
-          <Col md={4} className="mb-4 d-flex justify-content-center">
-            <div className="rectangle">
-              <span className="default-text">
-                KURSY DLA DZIECI W WIEKU PONIZEJ 6 LAT
-              </span>
-              <span className="hover-text">
-                Jest to kurs dla maluchów dzięki któremu zaznajomią się z
-                podstawowymi słówkami. Nauka opiera się na zabawie, reagowaniu,
-                powtarzaniu i naśladowaniu. Wszystko o odbywa się w przyjaznej
-                atmosferze, często w akompaniamencie prostych piosenek i
-                rymowanek w języku angielskim. Stopniowo wprowadzane jest też
-                tzw. czytanie globalne.
-              </span>
-            </div>
-          </Col>
-          <Col md={4} className="mb-4 d-flex justify-content-center">
-            <div className="rectangle">
-              <span className="default-text">
-                KURS DLA DZIECI W WIEKU SZKOLNYM
-              </span>
-              <span className="hover-text">
-                Dzieci przypisywane są do grup poziomowych na podstawie rozmowy
-                z lektorem oraz krótkiego testu pisemnego. Na każdych zajęciach
-                występuje co najmniej jedna forma zabawy, ale ćwiczone jest
-                również pisanie i czytanie w języku angielskim. Duży nacisk
-                kładziemy na mówienie, a więc zadaniem które pojawia się na
-                każdej lekcji jest przeprowadzanie dialogów tematycznych w
-                parach lub grupach. Oprócz tego dzieci ćwiczą osłuchiwanie się z
-                językiem angielskim za pomocą piosenek, rymowanek i nagrań z
-                książki.
-              </span>
-            </div>
-          </Col>
-          <Col md={4} className="mb-4 d-flex justify-content-center">
-            <div className="rectangle">
-              <span className="default-text">KURS DLA MŁODZIEZY</span>
-              <span className="hover-text">
-                Młodzież przypisywana jest do grup poziomowych na podstawie
-                rozmowy z lektorem oraz krótkiego testu pisemnego. Podczas pracy
-                z młodzieżą skupiamy się na mówieniu, a więc podobnie jak w
-                grupach dziecięcych, na każdej lekcji pojawia się co najmniej
-                jedno zadanie ćwiczące tę umiejętność. Zajmuje około 20% zajęć
-                niezależnie od poziomu grupy. Pozostały czas zajęć, na który
-                składa się słownictwo, gramatyka, słuchanie i pisanie,
-                dopasowujemy do potrzeb uczniów. Oznacza to, że niekiedy nauce
-                np. gramatyki lektor może poświęcić nawet 40% lekcji.{" "}
-              </span>
-            </div>
-          </Col>
-          <Col md={4} className="mb-4 d-flex justify-content-center">
-            <div className="rectangle">
-              <span className="default-text">
-                KURSY PRZYGOTOWUJĄCE DO EGZAMINÓW
-              </span>
-              <span className="hover-text">
-                Kurs przygotowujący do egzaminu ósmoklasisty i ten
-                przygotowujący do matury mają podobną strukturę. W przypadku
-                matury z języka angielskiego oferujemy przygotowanie do poziomu
-                podstawowego i rozszerzonego. Skupiamy się na przerabianiu zadań
-                typowo egzaminacyjnych, szczególną uwagę przywiązując do tych,
-                które sprawiają uczniom największą trudność. Na około 2 miesiące
-                przed egzaminem ćwiczymy arkusze egzaminacyjne. Część pisemna
-                egzaminu stanowi prace domową, która jest sprawdzana i
-                weryfikowana na następnej lekcji.{" "}
-              </span>
-            </div>
-          </Col>
-          <Col md={4} className="mb-4 d-flex justify-content-center">
-            <div className="rectangle">
-              <span className="default-text">KURS DLA DOROSŁYCH</span>
-              <span className="hover-text">
-                Osoby dorosłe przypisujemy do konkretnego poziomu po rozmowie z
-                lektorem. Zajęcia dla dorosłych mają podobną strukturę jak te
-                dla młodzieży. Ćwiczymy mówienie, pisanie, słuchanie, gramatykę
-                i czytanie.*{" "}
-              </span>
-              <p className="hover-text">*nie dotyczy zajęć konwersacyjnych</p>
-            </div>
-          </Col>
+          <p className="lead mb-5" style={{ width: "70%" }}>
+            Zajęcia na każdym poziomie zaawansowania{" "}
+            <span style={{ fontWeight: "bold" }}>
+              odbywają się wyłącznie w języku angielskim
+            </span>{" "}
+            po to, aby każdy zanurzył się w nim jak tylko jest to możliwe ☺
+            Dzieci powyżej 6 lat,
+            <span style={{ fontWeight: "bold" }}>
+              młodzież oraz dorośli przypisywani są do grup poziomowych
+            </span>{" "}
+            na podstawie rozmowy z lektorem i/lub krótkiego testu pisemnego.
+          </p>
+          {[
+            {
+              title: "KURSY DLA DZIECI W WIEKU PONIŻEJ 6 LAT",
+              text: "Jest to kurs dla maluchów, który opiera się na zabawie, reagowaniu, powtarzaniu i naśladowaniu, a więc za pomocą metody TPR. Wszystko to odbywa się w przyjaznej atmosferze, zwykle w akompaniamencie prostych piosenek i rymowanek w języku angielskim. Stopniowo wprowadzane jest czytanie globalne.",
+            },
+            {
+              title: "KURS DLA DZIECI W WIEKU SZKOLNYM",
+              text: "Dzieci przypisywane są do grup poziomowych na podstawie rozmowy z lektorem oraz krótkiego testu pisemnego. Na każdych zajęciach występuje co najmniej jedna forma zabawy, ale ćwiczone jest również pisanie i czytanie w języku angielskim. Duży nacisk kładziemy na mówienie, a więc zadaniem które pojawia się na każdej lekcji jest przeprowadzanie dialogów tematycznych w parach lub grupach. Oprócz tego dzieci ćwiczą osłuchiwanie się z językiem angielskim za pomocą piosenek, rymowanek i nagrań z książki.",
+            },
+            {
+              title: "KURS DLA MŁODZIEŻY",
+              text: "Na każdych zajęciach występuje co najmniej jedna forma zabawy, ale ćwiczone jest również pisanie i czytanie w języku angielskim. Duży nacisk kładziemy na mówienie, a więc zadaniem które pojawia się na każdej lekcji jest przeprowadzanie dialogów tematycznych w parach lub grupach. Oprócz tego dzieci ćwiczą osłuchiwanie się z językiem angielskim za pomocą piosenek, rymowanek i nagrań.",
+            },
+            {
+              title: "KURSY PRZYGOTOWUJĄCE DO EGZAMINÓW",
+              text: "Kurs przygotowujący do egzaminu ósmoklasisty i ten przygotowujący do matury mają podobną strukturę. W przypadku matury z języka angielskiego oferujemy przygotowanie do poziomu podstawowego i rozszerzonego. Skupiamy się na przerabianiu zadań typowo egzaminacyjnych oraz poznawaniu strategii które pozwolą wejść na salę egzaminacyjną bez stresu i uzyskać zadowalający wynik ☺",
+            },
+            {
+              title: "KURS DLA DOROSŁYCH",
+              text: "Zajęcia te mają podobną strukturę jak te dla młodzieży, ale z planem nauczania zadedykowanym osobom dorosłym. Pojawiają się tematy „na czasie”, co sprawia, że każdy kursant może się swobodnie wypowiedzieć, a w konsekwencji bariera językowa szybko idzie w zapomnienie. *nie dotyczy zajęć konwersacyjnych",
+            },
+          ].map((course, index) => (
+            <Col key={index} md={4} className="mb-4 d-flex justify-content-center">
+              <div className="rectangle">
+                <span className="default-text">{course.title}</span>
+                <span className="hover-text">{course.text}</span>
+              </div>
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
