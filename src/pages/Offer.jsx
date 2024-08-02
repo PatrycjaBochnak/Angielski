6db99dfb1c1af48be4a73b90f9e3282b35ff9e96
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-08-02 13:34:13 +0200

    code

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -8,12 +8,12 @@ const Offer = () => {
     <div
       name="offer"
       className="d-flex flex-column justify-content-center align-items-center component-style"
-      style={{ height: "150vh" }}
+      style={{ height: "170vh" }}
     >
       <h1 className="heading-custom mb-5 mt-5">Oferta</h1>
       <Container>
         <Row className="justify-content-center">
-          <p className="lead mb-5" style={{ width: "70%" }}>
+          <p className="lead mb-5 text-center" style={{ width: "70%" }}>
             Zajęcia na każdym poziomie zaawansowania{" "}
             <span style={{ fontWeight: "bold" }}>
               odbywają się wyłącznie w języku angielskim
@@ -32,19 +32,19 @@ const Offer = () => {
             },
             {
               title: "KURS DLA DZIECI W WIEKU SZKOLNYM",
-              text: "Dzieci przypisywane są do grup poziomowych na podstawie rozmowy z lektorem oraz krótkiego testu pisemnego. Na każdych zajęciach występuje co najmniej jedna forma zabawy, ale ćwiczone jest również pisanie i czytanie w języku angielskim. Duży nacisk kładziemy na mówienie, a więc zadaniem które pojawia się na każdej lekcji jest przeprowadzanie dialogów tematycznych w parach lub grupach. Oprócz tego dzieci ćwiczą osłuchiwanie się z językiem angielskim za pomocą piosenek, rymowanek i nagrań z książki.",
-            },
-            {
-              title: "KURS DLA MŁODZIEŻY",
               text: "Na każdych zajęciach występuje co najmniej jedna forma zabawy, ale ćwiczone jest również pisanie i czytanie w języku angielskim. Duży nacisk kładziemy na mówienie, a więc zadaniem które pojawia się na każdej lekcji jest przeprowadzanie dialogów tematycznych w parach lub grupach. Oprócz tego dzieci ćwiczą osłuchiwanie się z językiem angielskim za pomocą piosenek, rymowanek i nagrań.",
             },
             {
+              title: "KURS DLA MŁODZIEŻY",
+              text: "Podczas pracy z młodzieżą skupiamy się na mówieniu, a więc podobnie jak w grupach dziecięcych, na każdej lekcji  pojawia się co najmniej jedno zadanie ćwiczące tę umiejętność. Zajmuje około 20% zajęć niezależnie od poziomu grupy. Pozostały czas zajęć, na który składa się słownictwo, gramatyka, słuchanie i pisanie, dopasowujemy do potrzeb uczniów. Oznacza to, że niekiedy nauce np. gramatyki lektor może poświęcić nawet 40% lekcji.",
+            },
+            {
               title: "KURSY PRZYGOTOWUJĄCE DO EGZAMINÓW",
-              text: "Kurs przygotowujący do egzaminu ósmoklasisty i ten przygotowujący do matury mają podobną strukturę. W przypadku matury z języka angielskiego oferujemy przygotowanie do poziomu podstawowego i rozszerzonego. Skupiamy się na przerabianiu zadań typowo egzaminacyjnych oraz poznawaniu strategii które pozwolą wejść na salę egzaminacyjną bez stresu i uzyskać zadowalający wynik ☺",
+              text: "Kurs przygotowujący do egzaminu ósmoklasisty i ten przygotowujący do matury mają podobną strukturę. W przypadku matury z języka angielskiego oferujemy przygotowanie do poziomu podstawowego i rozszerzonego. Skupiamy się na przerabianiu zadań typowo egzaminacyjnych oraz poznawaniu strategii które pozwolą wejść na salę egzaminacyjną bez stresu i uzyskać zadowalający wynik.",
             },
             {
               title: "KURS DLA DOROSŁYCH",
-              text: "Zajęcia te mają podobną strukturę jak te dla młodzieży, ale z planem nauczania zadedykowanym osobom dorosłym. Pojawiają się tematy „na czasie”, co sprawia, że każdy kursant może się swobodnie wypowiedzieć, a w konsekwencji bariera językowa szybko idzie w zapomnienie. *nie dotyczy zajęć konwersacyjnych",
+              text: "Zajęcia te mają podobną strukturę jak te dla młodzieży, ale z planem nauczania zadedykowanym osobom dorosłym. Pojawiają się tematy „na czasie”, co sprawia, że każdy kursant może się swobodnie wypowiedzieć, a w konsekwencji bariera językowa szybko idzie w zapomnienie. *nie dotyczy zajęć konwersacyjnych.",
             },
           ].map((course, index) => (
             <Col key={index} md={4} className="mb-4 d-flex justify-content-center">
--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,27 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Reviews = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://static.elfsight.com/platform/platform.js";
+    script.setAttribute("data-use-service-core", "");
+    script.defer = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div
       name="reviews"
       className="d-flex flex-column justify-content-center align-items-center component-style"
-      style={{ height: "87vh" }}
     >
       <div className="container text-center">
         <div className="row">
           <div className="col-lg-8 mx-auto p-4">
-            <h1 className="heading-custom">
-              Tak o nas mówią
-            </h1>
-            <p className="lead">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <h1 className="heading-custom">Tak o nas mówią</h1>
+            <div
+              className="elfsight-app-5d61179c-11d8-4fb5-a6c4-653ca21eacd1"
+              data-elfsight-app-lazy
+            ></div>
           </div>
         </div>
       </div>
