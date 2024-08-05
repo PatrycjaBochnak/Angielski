31002c624864285242a402f6a80965a47ac4fe9a
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-08-05 10:37:35 +0200

    final version

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,60 +6,59 @@ import Footer from "../layout/Footer";
 
 const Contact = () => {
   return (
-    <div name="contact"
-    style={{ minHeight: "85vh", marginTop: "115px" }}>
-    <div
-      className="d-flex flex-column justify-content-center align-items-center component-style"
-      style={{ minHeight: "80vh", marginBottom: "0px" }}
-    >
-      <div className="text-center">
-        <h1 className="heading-custom mb-5">Kontakt</h1>
-        <p className="lead text-gray-700 mb-3" style={{ width: "700px" }}>
-          Wypełnij formularz kontaktowy by wysłać bezpośrednio do mnie wiadomość
-          lub skontaktuj się ze mną za pomocą maila -{" "}
-          <span className="font-weight-bold" style={{ fontWeight: "bold" }}>
-            biuro@szkolainsideout.pl
-          </span>
-        </p>
-      </div>
-      <div className="container mt-5">
-        <div className="row justify-content-center">
-          <div className="col-lg-3">
-            <div className="d-flex flex-column" style={{ fontSize: "20px" }}>
-              <p className="fw-bold" style={{ fontSize: "30px" }}>
-                Kontakt
-              </p>
-              <p>
-                {" "}
-                <Icons.TelephoneFill /> +48 727-938-163
-              </p>
-              <p>
-                {" "}
-                <Icons.EnvelopeArrowUpFill /> biuro@szkolainsideout.pl
-              </p>
-              <p className="fw-bold" style={{ fontSize: "30px" }}>
-                InsideOut English Academy
-              </p>
-              <p>
-                {" "}
-                <Icons.GeoAltFill /> ul. Wielkopolska 68 lokal U5
-              </p>
-              <p style={{ marginLeft: "25px" }}>80-180 Gdańsk</p>
+    <div name="contact" style={{ minHeight: "85vh", marginTop: "115px" }}>
+      <div
+        className="d-flex flex-column justify-content-center align-items-center component-style"
+        style={{ minHeight: "80vh", marginBottom: "0px" }}
+      >
+        <div className="text-center">
+          <h1 className="heading-custom mb-5">Kontakt</h1>
+          <p className="lead text-gray-700 mb-3" style={{ width: "700px" }}>
+            Wypełnij formularz kontaktowy by wysłać bezpośrednio do mnie
+            wiadomość lub skontaktuj się ze mną za pomocą maila -{" "}
+            <span className="font-weight-bold" style={{ fontWeight: "bold" }}>
+              biuro@szkolainsideout.pl
+            </span>
+          </p>
+        </div>
+        <div className="container mt-5">
+          <div className="row justify-content-center">
+            <div className="col-lg-3">
+              <div className="d-flex flex-column" style={{ fontSize: "20px" }}>
+                <p className="fw-bold" style={{ fontSize: "30px" }}>
+                  Kontakt
+                </p>
+                <p>
+                  {" "}
+                  <Icons.TelephoneFill /> +48 727-938-163
+                </p>
+                <p>
+                  {" "}
+                  <Icons.EnvelopeArrowUpFill /> biuro@szkolainsideout.pl
+                </p>
+                <p className="fw-bold" style={{ fontSize: "30px" }}>
+                  InsideOut English Academy
+                </p>
+                <p>
+                  {" "}
+                  <Icons.GeoAltFill /> ul. Wielkopolska 68 lokal U5
+                </p>
+                <p style={{ marginLeft: "25px" }}>80-180 Gdańsk</p>
+              </div>
             </div>
-          </div>
-          <div className="col-lg-6 mt-4 mt-lg-0">
-            <form
-              method="POST"
-              action="https://getform.io/f/agdylwrb"
-              className="max-w-[600px] w-full"
-            >
-              <ContactForm />
-            </form>
+            <div className="col-lg-6 mt-4 mt-lg-0">
+              <form
+                method="POST"
+                action="https://getform.io/f/alljejla"
+                className="max-w-[600px] w-full"
+              >
+                <ContactForm />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Footer /> 
+      <Footer />
     </div>
   );
 };
--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -2,6 +2,39 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import dorosli from "../asstes/photos/dorosli.jpg";
+import mlodziez from "../asstes/photos/mlodziez.jpg";
+import egzaminy from "../asstes/photos/egzaminy.jpg";
+import dzieciPonizej6Lat from "../asstes/photos/dzieciPonizej6Lat.jpg";
+import dzieciWiekSzkolny from "../asstes/photos/dzieciWiekSzkolny.jpg";
+
+const courses = [
+  {
+    title: "KURS DLA DZIECI W WIEKU PONIŻEJ 6 LAT",
+    text: "Jest to kurs dla maluchów, który opiera się na zabawie, reagowaniu, powtarzaniu i naśladowaniu, a więc za pomocą metody TPR. Wszystko to odbywa się w przyjaznej atmosferze, zwykle w akompaniamencie prostych piosenek i rymowanek w języku angielskim. Stopniowo wprowadzane jest czytanie globalne.",
+    image: dzieciPonizej6Lat,
+  },
+  {
+    title: "KURS DLA DZIECI W WIEKU SZKOLNYM",
+    text: "Na każdych zajęciach występuje co najmniej jedna forma zabawy, ale ćwiczone jest również pisanie i czytanie w języku angielskim. Duży nacisk kładziemy na mówienie, a więc zadaniem które pojawia się na każdej lekcji jest przeprowadzanie dialogów tematycznych w parach lub grupach. Oprócz tego dzieci ćwiczą osłuchiwanie się z językiem angielskim za pomocą piosenek, rymowanek i nagrań.",
+    image: dzieciWiekSzkolny,
+  },
+  {
+    title: "KURS DLA MŁODZIEŻY",
+    text: "Podczas pracy z młodzieżą skupiamy się na mówieniu, a więc podobnie jak w grupach dziecięcych, na każdej lekcji  pojawia się co najmniej jedno zadanie ćwiczące tę umiejętność. Zajmuje około 20% zajęć niezależnie od poziomu grupy. Pozostały czas zajęć, na który składa się słownictwo, gramatyka, słuchanie i pisanie, dopasowujemy do potrzeb uczniów. Oznacza to, że niekiedy nauce np. gramatyki lektor może poświęcić nawet 40% lekcji.",
+    image: mlodziez,
+  },
+  {
+    title: "KURSY PRZYGOTOWUJĄCE DO EGZAMINÓW",
+    text: "Kurs przygotowujący do egzaminu ósmoklasisty i ten przygotowujący do matury mają podobną strukturę. W przypadku matury z języka angielskiego oferujemy przygotowanie do poziomu podstawowego i rozszerzonego. Skupiamy się na przerabianiu zadań typowo egzaminacyjnych oraz poznawaniu strategii które pozwolą wejść na salę egzaminacyjną bez stresu i uzyskać zadowalający wynik.",
+    image: egzaminy,
+  },
+  {
+    title: "KURS DLA DOROSŁYCH",
+    text: "Zajęcia te mają podobną strukturę jak te dla młodzieży, ale z planem nauczania zadedykowanym osobom dorosłym. Pojawiają się tematy „na czasie”, co sprawia, że każdy kursant może się swobodnie wypowiedzieć, a w konsekwencji bariera językowa szybko idzie w zapomnienie.",
+    image: dorosli,
+  },
+];
 
 const Offer = () => {
   return (
@@ -24,36 +57,16 @@ const Offer = () => {
             </span>{" "}
             na podstawie rozmowy z lektorem i/lub krótkiego testu pisemnego.
           </p>
-          {[
-            {
-              title: "KURS DLA DZIECI W WIEKU PONIŻEJ 6 LAT",
-              text: "Jest to kurs dla maluchów, który opiera się na zabawie, reagowaniu, powtarzaniu i naśladowaniu, a więc za pomocą metody TPR. Wszystko to odbywa się w przyjaznej atmosferze, zwykle w akompaniamencie prostych piosenek i rymowanek w języku angielskim. Stopniowo wprowadzane jest czytanie globalne.",
-            },
-            {
-              title: "KURS DLA DZIECI W WIEKU SZKOLNYM",
-              text: "Na każdych zajęciach występuje co najmniej jedna forma zabawy, ale ćwiczone jest również pisanie i czytanie w języku angielskim. Duży nacisk kładziemy na mówienie, a więc zadaniem które pojawia się na każdej lekcji jest przeprowadzanie dialogów tematycznych w parach lub grupach. Oprócz tego dzieci ćwiczą osłuchiwanie się z językiem angielskim za pomocą piosenek, rymowanek i nagrań.",
-            },
-            {
-              title: "KURS DLA MŁODZIEŻY",
-              text: "Podczas pracy z młodzieżą skupiamy się na mówieniu, a więc podobnie jak w grupach dziecięcych, na każdej lekcji  pojawia się co najmniej jedno zadanie ćwiczące tę umiejętność. Zajmuje około 20% zajęć niezależnie od poziomu grupy. Pozostały czas zajęć, na który składa się słownictwo, gramatyka, słuchanie i pisanie, dopasowujemy do potrzeb uczniów. Oznacza to, że niekiedy nauce np. gramatyki lektor może poświęcić nawet 40% lekcji.",
-            },
-            {
-              title: "KURSY PRZYGOTOWUJĄCE DO EGZAMINÓW",
-              text: "Kurs przygotowujący do egzaminu ósmoklasisty i ten przygotowujący do matury mają podobną strukturę. W przypadku matury z języka angielskiego oferujemy przygotowanie do poziomu podstawowego i rozszerzonego. Skupiamy się na przerabianiu zadań typowo egzaminacyjnych oraz poznawaniu strategii które pozwolą wejść na salę egzaminacyjną bez stresu i uzyskać zadowalający wynik.",
-            },
-            {
-              title: "KURS DLA DOROSŁYCH",
-              text: "Zajęcia te mają podobną strukturę jak te dla młodzieży, ale z planem nauczania zadedykowanym osobom dorosłym. Pojawiają się tematy „na czasie”, co sprawia, że każdy kursant może się swobodnie wypowiedzieć, a w konsekwencji bariera językowa szybko idzie w zapomnienie.",
-            },
-          ].map((course, index) => (
+          {courses.map((course, index) => (
             <Col
               key={index}
               md={4}
               className="mb-4 d-flex justify-content-center"
             >
               <div className="rectangle">
-                <span className="default-text">{course.title}</span>
-                <span className="hover-text">{course.text}</span>
+                <img src={course.image} alt={course.title} />
+                <span className="title-text">{course.title}</span>
+                <div className="hover-text">{course.text}</div>
               </div>
             </Col>
           ))}
